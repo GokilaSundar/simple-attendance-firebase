0db9de1e4ecf95fd4aff78f37d63680f5422a389
GokilaSundar <gokithilaga267@gmail.com>
2025-05-07 00:49:21 +0530

    Add preloader with similar UI to CircularProgress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useLayoutEffect } from "react";
 import "./App.css";
 import { AuthProvider } from "./components";
 import { Pages } from "./pages";
 
 export const App = () => {
+  useLayoutEffect(() => {
+    const preloader = document.getElementById("preloader");
+
+    if (preloader) {
+      preloader.remove();
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Pages />
