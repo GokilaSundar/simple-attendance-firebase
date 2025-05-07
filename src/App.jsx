675edbf5701c3eedd3649e355b0cefb52a3177ff
GokilaSundar <gokithilaga267@gmail.com>
2025-05-07 09:26:23 +0530

    Add theme provider from Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import { createTheme, ThemeProvider } from "@mui/material";
 import { useLayoutEffect } from "react";
 
 import { AuthProvider, ConfigProvider } from "./components";
 import { Pages } from "./pages";
+
+const theme = createTheme();
 
 export const App = () => {
   useLayoutEffect(() => {
@@ -17,7 +20,9 @@ export const App = () => {
   return (
     <AuthProvider>
       <ConfigProvider>
-        <Pages />
+        <ThemeProvider theme={theme}>
+          <Pages />
+        </ThemeProvider>
       </ConfigProvider>
     </AuthProvider>
   );
