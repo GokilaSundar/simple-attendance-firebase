61ee83e6f5cf4cf1f6b5cbfc6a7c704757aa045c
GokilaSundar <gokithilaga267@gmail.com>
2025-05-07 08:22:15 +0530

    Install and setup DatePickers from Material UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App.jsx";
 import { CssBaseline } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <CssBaseline />
-      <App />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <App />
+      </LocalizationProvider>
     </BrowserRouter>
   </StrictMode>
 );
